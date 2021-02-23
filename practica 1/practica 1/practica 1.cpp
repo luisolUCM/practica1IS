bb70cdb3b1099a823ed0bc8568502748b0015926
Luis Solis <luisolis@ucm.es>
2021-02-23 10:40:31 +0100

    CMakeLists.txt

---
+++ practica 1/practica 1/practica 1.cpp
@@ -0,0 +1,12 @@
+﻿// practica 1.cpp: define el punto de entrada de la aplicación.
+//
+
+#include "practica 1.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
